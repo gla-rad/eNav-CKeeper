076fd612a3a3d1aa5e69a6e5082ee4ba1babacfa
Nikolaos Vastardis <n.vastardis@gmail.com>
2024-10-29 12:48:44 +0000

    Upgrading the Springboot version, along with bootstrap and datatables

--- src/main/resources/static/src/mrnEntities.js
+++ src/main/resources/static/src/mrnEntities.js
@@ -130,7 +130,7 @@ $(() => {
             }
         },
         columns: mrnEntitiesColumnDefs,
-        dom: "<'row'<'col-lg-2 col-md-4'B><'col-lg-2 col-md-4'l><'col-lg-8 col-md-4'f>><'row'<'col-md-12'rt>><'row'<'col-md-6'i><'col-md-6'p>>",
+        dom: '<"d-flex"<"flex-start"B><"flex-middle p-1"l><"flex-end flex-fill"f>><"d-flex mt-1 mb-1"t><"d-flex w-100"<"flex-fill"i><"flex-end"p>>',
         select: 'single',
         lengthMenu: [10, 25, 50, 75, 100],
         responsive: true,
@@ -312,7 +312,7 @@ function loadMrnEntityCertificates(event, table, button, config) {
             }
         },
         columns: certificatesColumnDefs,
-        dom: "<'row'<'col-lg-2 col-md-4'B><'col-lg-2 col-md-4'l><'col-lg-8 col-md-4'f>><'row'<'col-md-12'rt>><'row'<'col-md-6'i><'col-md-6'p>>",
+        dom: '<"d-flex"<"flex-start"B><"flex-middle p-1"l><"flex-end flex-fill"f>><"d-flex mt-1 mb-1"t><"d-flex w-100"<"flex-fill"i><"flex-end"p>>',
         select: 'single',
         autoWidth: false,
         lengthMenu: [10, 25, 50, 75, 100],
